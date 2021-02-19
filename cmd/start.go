e651526d0616e2ea158cdc6d14519a24b0e836da
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2021-02-19 02:08:00 +0100

    Redirect via URL query string

---
+++ cmd/start.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/tdex-network/tor-proxy/pkg/torproxy"
+	"github.com/urfave/cli/v2"
+)
+
+var start = cli.Command{
+	Name:  "start",
+	Usage: "start the reverse proxy",
+	Flags: []cli.Flag{
+		&cli.StringFlag{
+			Name:     "registry",
+			Usage:    "JSON file or string with list of onion endpoints. For more info see https://github.com/TDex-network/tdex-registry",
+			Required: true,
+		},
+		&cli.StringFlag{
+			Name:  "domain",
+			Usage: "TLD domain to expose the reverse proxy",
+		},
+		&cli.BoolFlag{
+			Name:  "insecure",
+			Usage: "expose in plaintext in localhost",
+			Value: true,
+		},
+		&cli.IntFlag{
+			Name:  "port",
+			Usage: "listening port for the reverse proxy",
+			Value: 7070,
+		},
+		&cli.StringFlag{
+			Name:  "socks5_hostname",
+			Usage: "the socks5 hostname exposed by the tor client",
+			Value: "127.0.0.1",
+		},
+		&cli.IntFlag{
+			Name:  "socks5_port",
+			Usage: "the socks5 port exposed by the tor client",
+			Value: 9150,
+		},
+	},
+	Action: startAction,
+}
+
+func startAction(ctx *cli.Context) error {
+
+	// will check if domain is given, now we default to insecure
+	listeningAddress := "localhost:" + fmt.Sprint(ctx.Int("port"))
+
+	// registry
+	var data []map[string]string
+	err := json.Unmarshal([]byte(ctx.String("registry")), &data)
+	if err != nil {
+		return fmt.Errorf("invalid JSON: %w", err)
+	}
+	redirects := make([]string, 0)
+	for _, v := range data {
+		if strings.Contains(v["endpoint"], "onion") {
+			redirects = append(redirects, v["endpoint"])
+		}
+	}
+	if len(redirects) == 0 {
+		return fmt.Errorf("no onion endpoints found in registry")
+	}
+
+	// Serve the reverse proxy
+	proxy := torproxy.NewTorProxyFromHostAndPort(
+		listeningAddress,
+		ctx.String("socks5_hostname"),
+		ctx.Int("socks5_port"),
+	)
+	proxy.WithRedirects(redirects)
+
+	log.Printf("Serving tor proxy on %s\n", listeningAddress)
+	if err := proxy.Serve(); err != nil {
+		return fmt.Errorf("serving proxy: %w", err)
+	}
+
+	return nil
+}
