81b9308c0129e799a727a8a3ab25e4d35e49ccaa
louisinger <louis@vulpem.com>
2021-11-26 14:56:26 +0100

    remove TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256

--- pkg/torproxy/torproxy.go
+++ pkg/torproxy/torproxy.go
@@ -177,7 +177,6 @@ func (tp *TorProxy) Serve(address string, options *TLSOptions) error {
 				Certificates: []tls.Certificate{certificate},
 				CipherSuites: []uint16{
 					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 					tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
 					tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
 					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
