4a32fb4b6dde904bea7d8432c7259fc0e35042ff
tiero <3596602+tiero@users.noreply.github.com>
2022-01-03 20:57:32 +0100

    TLS: set CipherSuites and MinVersion

--- pkg/torproxy/torproxy.go
+++ pkg/torproxy/torproxy.go
@@ -1,6 +1,7 @@
 package torproxy
 
 import (
+	"crypto/rand"
 	"crypto/tls"
 	"encoding/json"
 	"errors"
@@ -165,19 +166,15 @@ func (tp *TorProxy) Serve(address string, options *TLSOptions) error {
 			}
 
 			tlsConfig = &tls.Config{
+				MinVersion:   tls.VersionTLS12,
 				NextProtos:   []string{"http/1.1", http2.NextProtoTLS, "h2-14"}, // h2-14 is just for compatibility. will be eventually removed.
 				Certificates: []tls.Certificate{certificate},
 				CipherSuites: []uint16{
 					tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
 					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-					tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-					tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-					tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-					tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-					tls.TLS_RSA_WITH_AES_256_CBC_SHA,
 				},
-				PreferServerCipherSuites: true,
 			}
+			tlsConfig.Rand = rand.Reader
 		} else {
 
 			// read and agree to your CA's legal documents
