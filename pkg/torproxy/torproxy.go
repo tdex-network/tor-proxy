2c00d7b9b5dea4791a019183e7f8c431aced0e59
altafan <18440657+altafan@users.noreply.github.com>
2022-09-26 15:26:00 +0200

    Revert "Add cors headers to every response (#19)"
    This reverts commit 87b060136aaf2aa2b18d57c9f6d743b19a5b359b.

--- pkg/torproxy/torproxy.go
+++ pkg/torproxy/torproxy.go
@@ -261,7 +261,7 @@ func reverseProxy(redirects []*url.URL, lis net.Listener, dialer proxy.Dialer) e
 		http.HandleFunc(removeForUpstream+"/", func(w http.ResponseWriter, r *http.Request) {
 
 			// add cors headers
-			addCorsHeader(w)
+			addCorsHeader(w, r)
 
 			// prepare request removing useless headers
 			if err := prepareRequest(r); err != nil {
@@ -286,7 +286,10 @@ func withoutOnion(host string) string {
 	return strings.ReplaceAll(hostWithoutPort, ".onion", "")
 }
 
-func addCorsHeader(w http.ResponseWriter) {
+func addCorsHeader(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodOptions {
+		return
+	}
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
