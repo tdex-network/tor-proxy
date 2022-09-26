c69a34c748775a7f46cac3a419a2d7e31b66e4cc
altafan <18440657+altafan@users.noreply.github.com>
2022-09-26 13:13:22 +0200

    Add cors headers to every response

--- pkg/torproxy/torproxy.go
+++ pkg/torproxy/torproxy.go
@@ -261,7 +261,7 @@ func reverseProxy(redirects []*url.URL, lis net.Listener, dialer proxy.Dialer) e
 		http.HandleFunc(removeForUpstream+"/", func(w http.ResponseWriter, r *http.Request) {
 
 			// add cors headers
-			addCorsHeader(w, r)
+			addCorsHeader(w)
 
 			// prepare request removing useless headers
 			if err := prepareRequest(r); err != nil {
@@ -286,10 +286,7 @@ func withoutOnion(host string) string {
 	return strings.ReplaceAll(hostWithoutPort, ".onion", "")
 }
 
-func addCorsHeader(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodOptions {
-		return
-	}
+func addCorsHeader(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "*")
